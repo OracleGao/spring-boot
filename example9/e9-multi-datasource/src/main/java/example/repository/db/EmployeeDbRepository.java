285f11f573b15cd5c2c6f8f68d1eeaa6195266c7
oraclegaomvnrepo <oraclegaomvnrepo@126.com>
2017-04-11 10:28:11 +0800

    fix invalidation anno

--- example9/e9-multi-datasource/src/main/java/example/repository/db/EmployeeDbRepository.java
+++ example9/e9-multi-datasource/src/main/java/example/repository/db/EmployeeDbRepository.java
@@ -2,14 +2,9 @@ package example.repository.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import example.entity.Employee;
 
-@RestController
-@RequestMapping(path = "/employees/db", produces = MediaType.APPLICATION_JSON_VALUE)
 public interface EmployeeDbRepository extends JpaRepository<Employee, String>, JpaSpecificationExecutor<Employee> {
 	
 }
